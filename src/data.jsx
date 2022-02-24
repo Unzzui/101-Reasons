cf3ed64e669d69475d083e3afa51c19cf4c36b42
Diego Bravo Becerra <diegobravo@Diegos-MacBook-Air.local>
2022-02-23 22:14:46 -0300

    adsa

--- src/data.jsx
+++ src/data.jsx
@@ -13,141 +13,147 @@ export const ReasonsData = [
     },
     {
         id: 4,
-        msg: '4.- Esa carita hermosa esculpida por los mismos dioses'
+        msg: '4.- "Esa carita hermosa esculpida por los mismos dioses."'
     },
     {
         id: 5,
-        msg: '5.- Te amo porque nunca me fallaste a pesar de las dificultades..'
+        msg: '5.- "Te amo porque nunca me fallaste a pesar de las dificultades..."'
     },
     {
         id: 6,
-        msg: '6.- Te amo porque a pesar de ser odioso, me aceptas tal cual soy.'
+        msg: '6.- "Te amo porque a pesar de ser odioso, me aceptas tal cual soy."'
     },
     {
         id: 7,
-        msg: '7.- Te amo porque eres autentica y te muestras tal como eres.'
+        msg: '7.- "Te amo porque eres autentica y te muestras tal como eres."'
     },
     {
         id: 8,
-        msg: '8.- Te amo por la forma en que me miras.'
+        msg: '8.- "Te amo por la forma en que me miras."'
     },
     {
         id: 9,
-        msg: '9.- Te amo porque cada dia me enseñas algo nuevo.'
+        msg: '9.- "Te amo porque cada dia me enseñas algo nuevo."'
     },
     {
         id: 10,
-        msg: '10.- Te amo porque me cuidas, me proteges.'
+        msg: '10.- "Te amo porque me cuidas, me proteges."'
     },
     {
         id: 11,
-        msg: '11.- Te amo 3000!!!'
+        msg: '11.- "Te amo 3000!!!"'
     },
     {
         id: 12,
-        msg: '12.- Te amo porque te admiro, eres unica!'
+        msg: '12.- "Te amo porque te admiro, eres unica!"'
     },
     {
         id: 13,
-        msg: '13.- Te amo porque das los besos me llevan a otra galaxia.'
+        msg: '13.- "Te amo porque das los besos me llevan a otra galaxia."'
     },
     {
         id: 14,
-        msg: '14.-  Te amo porque cada dia me sorprendes'
+        msg: '14.-  "Te amo porque cada dia me sorprendes."'
     },
     {
         id: 15,
-        msg: '15.- Te amo porque somos el mejor equipo que puede existir.'
+        msg: '15.- "Te amo porque somos el mejor equipo que puede existir."'
     },
     {
         id: 16,
-        msg: '16.- Te amo porque te extraño cuando me dejas despues de vernos.'
+        msg: '16.- "Te amo porque te extraño cuando me dejas despues de vernos."'
     }, 
     {
         id: 17,
-        msg: '17.- Te amo por ser mi singularidad, perderme en el tiempo estando contigo es algo unico.'
+        msg: '17.- "Te amo por ser mi singularidad, perderme en el tiempo estando contigo es algo unico."'
     },
     {
         id: 18,
-        msg: '18.- Te amo porque sabes como me siento y me entiendes.'
+        msg: '18.- "Te amo porque sabes como me siento y me entiendes."'
 
     },
     {
         id: 19,
-        msg: '19.- Te amo porque sacas lo mejor de mi.'
+        msg: '19.- "Te amo porque sacas lo mejor de mi."'
         
     },
     {
         id: 20,
-        msg: '20.- Te amo por la forma en que ves el mundo.'
+        msg: '20.- "Te amo por la forma en que ves el mundo."'
         
     },
     {
         id: 21,
-        msg: '21.- Amo tu cuerpo , amo cada centimetro de ti.'
+        msg: '21.- "Amo tu cuerpo , amo cada centimetro de ti."'
 
     },
 
     {
         id: 22,
-        msg: '22.- Amo tus ojitos preciosos que me llevan al mejor lugar del universo.'
+        msg: '22.- "Amo tus ojitos preciosos que me llevan al mejor lugar del universo."'
 
     },
     {
         id: 23,
-        msg: '23.- Amo la forma en que me acaricias, esa delicadez, esa ternura.'
+        msg: '23.- "Amo la forma en que me acaricias, esa delicadez, esa ternura."'
 
     },
     {
         id: 24,
-        msg: '24.- Te amo por querer formar una familia conmigo.'
+        msg: '24.- "Te amo por querer formar una familia conmigo."'
 
     },
     {
         id: 25,
-        msg: '25.- Te amo por ser una mujer esforzada y que no depende de nadie...'
+        msg: '25.- "Te amo por ser una mujer esforzada y que no depende de nadie..."'
 
     },
     {
         id: 26,
-        msg: '26.- Te amo por tu resiliencia...'
+        msg: '26.- "Te amo por tu resiliencia..."'
 
     },
     {
         id: 27,
-        msg: '27.- Te amo porque eres la mujer mas especial que pude conocer.'
+        msg: '27.- "Te amo porque eres la mujer mas especial que pude conocer."'
 
     },
     {
         id: 28,
-        msg: '28.- Te amo por ser la mujer de mis sueños.'
+        msg: '28.- "Te amo por ser la mujer de mis sueños."'
 
     },
     {
         id: 29,
-        msg: '29.- Te amo por ser una mujer soñadora.'
+        msg: '29.- "Te amo por ser una mujer soñadora."'
 
     },
     {
         id: 30,
-        msg: '30.-  Te amo por abrirte hacia mi a pesar de tener miedo de lo que pueda pasar...'
+        msg: '30.-  "Te amo por abrirte hacia mi a pesar de tener miedo de lo que pueda pasar..."'
 
     },
     {
         id: 31,
-        msg: '31.-  Te amo por saltar conmigo a pesar de las consecuencias.'
+        msg: '31.-  "Te amo por saltar conmigo a pesar de las consecuencias."'
 
     },
     {
         id: 32,
-        msg: '32.- Te amo por el amor infinito que me provocas con tus abrazos.'
+        msg: '32.- "Te amo por el amor infinito que me provocas con tus abrazos."'
 
     },
     {
         id: 33,
-        msg: '33.-  Te amo por la paz que me brindas al tomar mi mano.'
+        msg: '33.- "Te amo por la paz que me brindas al tomar mi mano."'
 
     },
+    {
+        id: 34,
+        msg: '34.- "Te amo por que seras la madre de nuestros hijos."'
+
+    },
+
 
 
 
