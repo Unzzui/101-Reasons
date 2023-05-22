88513acfe08d18565cb1a08f38f002526d54c030
Unzzui <98267393+Unzzui@users.noreply.github.com>
2023-05-21 22:25:29 -0400

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -153,10 +153,273 @@ export const ReasonsData = [
         msg: '34.- "Te amo por que seras la madre de nuestros hijos."'
 
     },
-
-
-
-
-
+    {
+    id: 35,
+    msg: '35.- "Te amo porque eres mi compañera de aventuras y risas."'
+},
+{
+    id: 36,
+    msg: '36.- "Te amo porque me haces sentir completo/a en todos los sentidos."'
+},
+{
+    id: 37,
+    msg: '37.- "Te amo porque cada día a tu lado es un regalo."'
+},
+{
+    id: 38,
+    msg: '38.- "Te amo porque siempre encuentras la manera de iluminar mi día."'
+},
+{
+    id: 39,
+    msg: '39.- "Te amo porque eres mi refugio en los momentos difíciles."'
+},
+{
+    id: 40,
+    msg: '40.- "Te amo porque me inspiras a ser una mejor persona."'
+},
+{
+    id: 41,
+    msg: '41.- "Te amo porque cada beso tuyo es como un hechizo de amor."'
+},
+{
+    id: 42,
+    msg: '42.- "Te amo porque me haces sentir único/a y especial."'
+},
+{
+    id: 43,
+    msg: '43.- "Te amo porque eres mi confidente y mi apoyo incondicional."'
+},
+{
+    id: 44,
+    msg: '44.- "Te amo porque siempre encuentras la manera de sacar una sonrisa de mi rostro."'
+},
+{
+    id: 45,
+    msg: '45.- "Te amo porque me haces sentir como si estuviera en un cuento de hadas."'
+},
+{
+    id: 46,
+    msg: '46.- "Te amo porque nuestra conexión es única y eterna."'
+},
+{
+    id: 47,
+    msg: '47.- "Te amo porque cada abrazo tuyo es como un refugio para mi alma."'
+},
+{
+    id: 48,
+    msg: '48.- "Te amo porque tu amor me da fuerzas para enfrentar cualquier desafío."'
+},
+{
+    id: 49,
+    msg: '49.- "Te amo porque nuestros corazones laten al unísono."'
+},
+{
+    id: 50,
+    msg: '50.- "Te amo porque eres mi razón de ser y mi inspiración."'
+},
+{
+    id: 51,
+    msg: '51.- "Te amo porque con cada mirada tuya siento que el tiempo se detiene."'
+},
+{
+    id: 52,
+    msg: '52.- "Te amo porque nuestros sueños se entrelazan en un mismo horizonte."'
+},
+{
+    id: 53,
+    msg: '53.- "Te amo porque juntos/as somos invencibles."'
+},
+{
+    id: 54,
+    msg: '54.- "Te amo porque en tus brazos encuentro mi paz interior."'
+},
+{
+    id: 55,
+    msg: '55.- "Te amo porque tu amor es el mejor regalo que he recibido."'
+},
+{
+    id: 56,
+    msg: '56.- "Te amo porque nuestro amor es como una melodía perfecta."'
+},
+{
+    id: 57,
+    msg: '57.- "Te amo porque cada momento a tu lado es un recuerdo inolvidable."'
+},
+{
+    id: 58,
+    msg: '58.- "Te amo porque eres mi felicidad en estado puro."'
+},
+{
+    id: 59,
+    msg: '59.- "Te amo porque con cada palabra tuya siento que el mundo tiene sentido."'
+},
+{
+    id: 60,
+    msg: '60.- "Te amo porque eres la persona con la que quiero pasar el resto de mi vida."'
+},
+{
+    id: 61,
+    msg: '61.- "Te amo porque en tu sonrisa encuentro la magia más pura."'
+},
+{
+    id: 62,
+    msg: '62.- "Te amo porque me aceptas tal como soy, con mis virtudes y mis defectos."'
+},
+{
+    id: 63,
+    msg: '63.- "Te amo porque nuestra historia de amor es mi cuento favorito."'
+},
+{
+    id: 64,
+    msg: '64.- "Te amo porque en tus ojos encuentro el reflejo de mi felicidad."'
+},
+{
+    id: 65,
+    msg: '65.- "Te amo porque me haces creer en el poder del amor verdadero."'
+},
+{
+    id: 66,
+    msg: '66.- "Te amo porque cada día a tu lado es una aventura maravillosa."'
+},
+{
+    id: 67,
+    msg: '67.- "Te amo porque eres mi motivo para levantarme cada mañana."'
+},
+{
+    id: 68,
+    msg: '68.- "Te amo porque tu presencia llena de luz todos los rincones de mi vida."'
+},
+{
+    id: 69,
+    msg: '69.- "Te amo porque eres mi inspiración para superar todos los obstáculos."'
+},
+{
+    id: 70,
+    msg: '70.- "Te amo porque eres la persona con la que quiero compartir todos mis sueños."'
+},
+{
+    id: 71,
+    msg: '71.- "Te amo porque me enseñas el verdadero significado del amor incondicional."'
+},
+{
+    id: 72,
+    msg: '72.- "Te amo porque eres mi equilibrio en este caótico mundo."'
+},
+{
+    id: 73,
+    msg: '73.- "Te amo porque en tu voz encuentro la paz que tanto anhelo."'
+},
+{
+    id: 74,
+    msg: '74.- "Te amo porque en tu mirada encuentro mi hogar."'
+},
+{
+    id: 75,
+    msg: '75.- "Te amo porque en tus brazos encuentro mi refugio seguro."'
+},
+{
+    id: 76,
+    msg: '76.- "Te amo porque cada momento a tu lado es una bendición."'
+},
+{
+    id: 77,
+    msg: '77.- "Te amo porque me enseñas a amar sin reservas."'
+},
+{
+    id: 78,
+    msg: '78.- "Te amo porque con cada beso tuyo siento que vuelo hasta el cielo."'
+},
+{
+    id: 79,
+    msg: '79.- "Te amo porque eres la razón por la que mi corazón late con fuerza."'
+},
+{
+    id: 80,
+    msg: '80.- "Te amo porque en tus abrazos encuentro mi paz interior."'
+},
+{
+    id: 81,
+    msg: '81.- "Te amo porque con cada caricia tuya siento que el tiempo se detiene."'
+},
+{
+    id: 82,
+    msg: '82.- "Te amo porque eres mi ancla en medio de las tormentas."'
+},
+{
+    id: 83,
+    msg: '83.- "Te amo porque en tus sueños encuentro mis propias ilusiones."'
+},
+{
+    id: 84,
+    msg: '84.- "Te amo porque cada día a tu lado es una aventura mágica."'
+},
+{
+    id: 85,
+    msg: '85.- "Te amo porque en tu risa encuentro la melodía más hermosa."'
+},
+{
+    id: 86,
+    msg: '86.- "Te amo porque en tus palabras encuentro el consuelo que necesito."'
+},
+{
+    id: 87,
+    msg: '87.- "Te amo porque me enseñas a ver el mundo con ojos llenos de amor."'
+},
+{
+    id: 88,
+    msg: '88.- "Te amo porque eres mi confidente y mi mejor amigo/a."'
+},
+{
+    id: 89,
+    msg: '89.- "Te amo porque tu amor es mi mayor tesoro."'
+},
+{
+    id: 90,
+    msg: '90.- "Te amo porque cada momento a tu lado es un regalo que atesoro."'
+},
+{
+    id: 91,
+    msg: '91.- "Te amo porque eres el sueño hecho realidad que nunca supe que tenía."'
+},
+{
+    id: 92,
+    msg: '92.- "Te amo porque en tu sonrisa encuentro la esperanza en días grises."'
+},
+{
+    id: 93,
+    msg: '93.- "Te amo porque en tus brazos encuentro el consuelo que necesito."'
+},
+{
+    id: 94,
+    msg: '94.- "Te amo porque cada vez que te veo, mi corazón se llena de alegría."'
+},
+{
+    id: 95,
+    msg: '95.- "Te amo porque con cada beso tuyo siento que el mundo se detiene."'
+},
+{
+    id: 96,
+    msg: '96.- "Te amo porque en tus abrazos encuentro mi paz y tranquilidad."'
+},
+{
+    id: 97,
+    msg: '97.- "Te amo porque me haces sentir especial en cada momento."'
+},
+{
+    id: 98,
+    msg: '98.- "Te amo porque me aceptas tal como soy, sin condiciones."'
+},
+{
+    id: 99,
+    msg: '99.- "Te amo porque juntos/as somos invencibles ante cualquier adversidad."'
+},
+{
+    id: 100,
+    msg: '100.- "Te amo porque cada día a tu lado es una nueva oportunidad de amar."'
+},
+{
+    id: 101,
+    msg: '101.- "Te amo porque eres mi razón de ser y mi eterna compañera de vida."'
+},
 
     ];
